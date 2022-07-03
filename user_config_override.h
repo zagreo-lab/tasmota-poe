e8998e417f0cd1fdfe90e4fafb5ce795d7fa57a0
zagreo-lab <78954441+zagreo-lab@users.noreply.github.com>
2022-07-03 11:23:33 +0200

    Update user_config_override.h

--- user_config_override.h
+++ user_config_override.h
@@ -1,56 +1,18 @@
 /*
   user_config_override.h - user configuration overrides my_user_config.h for Tasmota
-  Zagreo 26.12.2021, 19.06.2022
+  Zagreo 26.12.2021, 19.06.2022, 03.07.2022
 */
 
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// Solo per Test
-#ifdef FIRMWARE_CC2652P2_POE
-  #warning **** Build: FIRMWARE FOR CC2652P2-POE ****
-  #undef CODE_IMAGE_STR
-  #define CODE_IMAGE_STR "CC2652P2_POE"
-
-  #undef  SERIAL_LOG_LEVEL
-  #define SERIAL_LOG_LEVEL LOG_LEVEL_NONE
-
-  #ifdef USE_DOMOTICZ
-  #undef USE_DOMOTICZ //  disable Domoticz support (+6k code, +0.3k mem)
-  #endif
-
-  #ifdef USE_HOME_ASSISTANT
-  #undef USE_HOME_ASSISTANT //  disable Home Assistant Discovery Support (+12k code, +6 bytes mem)
-  #endif
-
-  #define USE_ZIGBEE
-  #define USE_ZIGBEE_ZNP
-  #undef  USE_ZIGBEE_EZSP  // Ensure ZNP support and not EZSP
-  #define USE_ZIGBEE_CHANNEL  11                 // Zigbee Channel (11-26)
-
-  #define USE_UFILESYS
-
-  #ifdef OTA_URL
-  #undef OTA_URL
-  #define OTA_URL "http://test/release/CC2652P2_POE.bin"  // [OtaUrl]
-  #endif
-
-  // Olimex ESP32-POE
-  #define USE_ETHERNET
-  #undef  ETH_TYPE
-  #define ETH_TYPE 0 // ETH_PHY_LAN8720
-  #undef  ETH_CLKMODE
-  #define ETH_CLKMODE 3 // 3 = ETH_CLOCK_GPIO17_OUT 0 = ETH_CLOCK_GPIO0_IN
-  #undef  ETH_ADDRESS
-  #define ETH_ADDRESS 0 // PHY1
-#endif
-
-// 26.12.2021, 19.06.2022
-// platformio run -e tasmota32-CC2652P2-POE-BRIDGE 
+// 26.12.2021, 19.06.2022, 03.07.2022
+// per compilare dare il comando: platformio run -e tasmota32-CC2652P2-POE-BRIDGE
+//
 #ifdef FIRMWARE_CC2652P2_POE_BRIDGE
   #warning **** Build: FIRMWARE FOR CC2652P2-POE-BRIDGE ****
   #undef CODE_IMAGE_STR
-  #define CODE_IMAGE_STR "CC2652P2_POE_BRIDGE_11.0.1"
+  #define CODE_IMAGE_STR "CC2652P2_POE_BRIDGE"
 
 #undef  SERIAL_LOG_LEVEL
   #define SERIAL_LOG_LEVEL LOG_LEVEL_NONE
